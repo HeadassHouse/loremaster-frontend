16f31ad973fc4241b76c27b0a2ea10eca2879bef
Corey Rigney <coreyrigney@gmail.com>
2020-06-13 17:21:06 -0400

    Added switch between viewing and creation on /characters

---
+++ src/components/Characters/CharacterViewer/CharacterViewer.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class CharacterViewer extends Component{
+    render(){
+        return(
+            <div>
+                <div className="jumbotron text-center">
+                    <h1>Characters</h1>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default CharacterViewer;
--- src/components/Pages/Characters/Characters.jsx
+++ src/components/Pages/Characters/Characters.jsx
@@ -1,13 +1,50 @@
 import React, { Component } from 'react';
+import './Characters.css'
+import CharacterViewer from '../../Characters/CharacterViewer/CharacterViewer';
+import CharacterCreator from '../../Characters/CharacterCreator/CharacterCreator';
 
 class Characters extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            viewer : -1,
+            system: {}
+        }
+
+        this.setViewerChoice = this.setViewerChoice.bind(this);
+    }
+
+    setViewerChoice(number){
+        this.setState({
+            viewer: number,
+            system: this.state.system
+        });
+    }
+
     render(){
+        console.log(this.state.viewer);
+        if(this.state.viewer == -1){
+            //return choice between viewer and creator
+            return(
+                <ul className="viewerChoiceList">
+                    <li className="viewerChoice" onClick={() => this.setViewerChoice(1)}>Character Viewer</li>
+                    <li className="viewerChocie" onClick={() => this.setViewerChoice(0)}>Character Creator</li>
+                </ul>
+            );
+        }
+        else if(this.state.viewer == 0){
+            //return character creator (probably with choice of system first)
+            return(
+                <CharacterCreator system={this.state.system} />
+            );       
+        }
+        else if(this.state.viewer == 1){
+            return (
+                <CharacterViewer />
+            );
+        }
         return(
-            <div>
-                <div className="jumbotron text-center">
-                    <h1>Characters</h1>
-                </div>
-            </div>
+            <p>Error in state.viewer</p>
         );
     }
 }
